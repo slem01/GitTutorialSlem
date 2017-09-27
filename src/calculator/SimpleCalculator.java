b7b757ed4f847ac6c95555feca81807597c8107b
sallyeverlove <sallyeverlove@shelbys-mbp.dhcp.nd.edu>
2017-09-27 18:07:04 -0400

    changed return values for simplecalculaor

--- src/calculator/SimpleCalculator.java
+++ src/calculator/SimpleCalculator.java
@@ -5,25 +5,25 @@ public class SimpleCalculator implements ICalculator {
 	@Override
 	public double add(double a, double b) {
 		// TODO Auto-generated method stub
-		return 0;
+		return a + b;
 	}
 
 	@Override
 	public double sub(double a, double b) {
 		// TODO Auto-generated method stub
-		return 0;
+		return a - b;
 	}
 
 	@Override
 	public double div(double a, double b) {
 		// TODO Auto-generated method stub
-		return 0;
+		return a / b;
 	}
 
 	@Override
 	public double mult(double a, double b) {
 		// TODO Auto-generated method stub
-		return 0;
+		return a * b;
 	}
 
 }
