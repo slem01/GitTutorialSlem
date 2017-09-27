4b2a5c83009f4ecc4e1b59581b2213ae9507e219
sallyeverlove <sallyeverlove@shelbys-mbp.dhcp.nd.edu>
2017-09-27 17:37:54 -0400

    Initial commit

---
+++ src/calculator/ICalculator.java
@@ -0,0 +1,12 @@
+package calculator;
+
+public interface ICalculator {
+	
+	public double add(double a, double b);
+	
+	public double sub(double a, double b);
+	
+	public double div(double a, double b);
+	
+	public double mult(double a, double b);
+}
---
+++ src/calculator/SimpleCalculator.java
@@ -0,0 +1,29 @@
+package calculator;
+
+public class SimpleCalculator implements ICalculator {
+
+	@Override
+	public double add(double a, double b) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public double sub(double a, double b) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public double div(double a, double b) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public double mult(double a, double b) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
